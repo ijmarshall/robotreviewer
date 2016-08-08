f6b93eb8c97096b9df11d0c2dbd5699f7ae7e000
bwallace <byron.wallace@gmail.com>
2016-08-08 12:52:43 -0400

    merging

--- robotreviewer/config.py
+++ robotreviewer/config.py
@@ -4,10 +4,10 @@ RobotReviewer configuration file
 
 # grobid
 USE_GROBID = True
-GROBID_PATH = "/Users/iain/Code/grobid-grobid-parent-0.4.0"
+GROBID_PATH = "/Users/byron/dev/grobid" # "/Users/iain/Code/grobid-grobid-parent-0.4.0"
 GROBID_HOST = "http://localhost:8080/" # must have http:// to start with
-TEMP_PDF = "/Users/iain/Code/py/robotreviewer4/robotreviewer/textprocessing/pdftmp"
-TEMP_ZIP = "/Users/iain/Code/py/robotreviewer4/robotreviewer/textprocessing/ziptmp"
+TEMP_PDF = "/Users/byron/dev/robotreviewer3/robotreviewer/textprocessing/pdftmp"
+TEMP_ZIP = "/Users/byron/dev/robotreviewer3/robotreviewer/textprocessing/ziptmp"
 
 
 
