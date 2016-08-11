972af85de4e46d781441cb401ed6f0a3c8043106
Joel Kuiper <me@joelkuiper.eu>
2016-08-11 15:48:53 +0200

    Somewhat working report view

--- robotreviewer/static/scripts/views/report.jsx
+++ robotreviewer/static/scripts/views/report.jsx
@@ -4,52 +4,18 @@ define(function (require) {
   var React = require("react");
 
   var Dropzone = require("react-dropzone")
-  var FileUtil = require("spa/helpers/fileUtil");
   var _ = require("underscore");
-  var Q = require("Q");
   var $ = require("jquery");
 
-  var uploadUri = "/add_pdfs_to_db";
-  var synthesizeUri = "/synthesize_uploaded";
-
   var ReportView = React.createClass({
-    onDrop: function (files) {
-      var fd = new FormData();
-
-      _.forEach(files, function(file) {
-        fd.append('file', file);
-      });
-
-      $.ajax({
-        type: 'POST',
-        url: uploadUri,
-        data: fd,
-        contentType: false,
-        cache: false,
-        processData: false,
-        success: function(data) {
-          $.post(synthesizeUri, {}, function(data, status) {
-            console.log(data, status);
-          });
-        }
+    componentDidMount: function() {
+      var $frame = $(this.refs.frame);
+      var html = $.get("report_view/html", function(data) {
+        $frame.contents().find("body").html(data);
       });
     },
     render: function() {
-      return (
-          <div>
-          <Dropzone onDrop={this.onDrop}
-                    accept="application/pdf"
-                    disablePreview={true}
-                    activeClassName="dropzone-active"
-                    className="dropzone">
-              <div>
-              RobotReviewer helps to automate systematic reviews in Evidence Based Medicine.
-              <br />
-              Try dropping Randomized Controlled Trial PDFs here, or click to select files to upload!
-              </div>
-            </Dropzone>
-          </div>
-      );
+      return(<div className="report"><iframe ref="frame" /></div>);
     }
   });
 
