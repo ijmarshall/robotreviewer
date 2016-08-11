e3c399384a930a52b568fe784d0dab96ff0d4c57
Joel Kuiper <me@joelkuiper.eu>
2016-08-11 22:50:36 +0200

    add super secret key

--- robotreviewer/config.py
+++ robotreviewer/config.py
@@ -4,15 +4,5 @@ RobotReviewer configuration file
 
 # grobid
 USE_GROBID = True
-GROBID_PATH = "/Users/iain/Code/grobid-grobid-parent-0.4.0"
+GROBID_PATH = "/Users/joelkuiper/Repositories/grobid"
 GROBID_HOST = "http://localhost:8080/" # must have http:// to start with
-
-
-
-
-
-
-
-
-
-
