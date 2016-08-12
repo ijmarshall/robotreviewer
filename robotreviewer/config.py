771d835061665996870b9053d0a2bffc56be2a2f
Iain Marshall <mail@ijmarshall.com>
2016-08-12 13:48:54 +0100

    threaded spacy parsing

--- robotreviewer/config.py
+++ robotreviewer/config.py
@@ -8,7 +8,8 @@ GROBID_PATH = "/Users/iain/Code/grobid-grobid-parent-0.4.0"
 GROBID_HOST = "http://localhost:8080/" # must have http:// to start with
 GROBID_THREADS = 4 # max number of articles to send to Grobid at once
 
-
+# spacy
+SPACY_THREADS = 4
 
 
 
