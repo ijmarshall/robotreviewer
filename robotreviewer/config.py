2f70e8124f9960784b329fb796b5e1f8eaca7e8c
Iain Marshall <mail@ijmarshall.com>
2016-08-12 10:20:51 +0100

    added threads to config.py

--- robotreviewer/config.py
+++ robotreviewer/config.py
@@ -6,6 +6,8 @@ RobotReviewer configuration file
 USE_GROBID = True
 GROBID_PATH = "/Users/iain/Code/grobid-grobid-parent-0.4.0"
 GROBID_HOST = "http://localhost:8080/" # must have http:// to start with
+GROBID_THREADS = 4 # max number of articles to send to Grobid at once
+
 
 
 
