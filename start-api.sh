60dfc8f2f240832ce1884094c45406c862ff3310
Iain Marshall <mail@ijmarshall.com>
2020-01-07 20:14:42 +0000

    fixed the cpu api start file

--- start-api.sh
+++ start-api.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 MODEL_PATH="$(pwd)/robotreviewer/data"
-docker run --runtime=nvidia --name "robotreviewer" --volume ${MODEL_PATH}:/var/lib/deploy/robotreviewer/data  --env ROBOTREVIEWER_REST_API=true -d --restart="always" -p 127.0.0.1:5050:5000 robotreviewer
+docker run --name "robotreviewer" --volume ${MODEL_PATH}:/var/lib/deploy/robotreviewer/data  --env ROBOTREVIEWER_REST_API=true -d --restart="always" -p 127.0.0.1:5050:5000 robotreviewer
